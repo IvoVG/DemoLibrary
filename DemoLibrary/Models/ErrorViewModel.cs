3a3fb6e6a54004f07c6365af3618aa5951509221
Ivo <ivg_ho@abv.bg>
2022-04-19 20:10:26 +0300

    Add project files.

---
+++ DemoLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
